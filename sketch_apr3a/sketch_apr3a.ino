8ba1876a056088cc3fab0a83bf2d86a8be48accd
Korobokchek <gosha.korobitskiy@gmail.com>
2024-04-10 10:23:45 +0300

    Update sketch_apr3a.ino

--- sketch_apr3a/sketch_apr3a.ino
+++ sketch_apr3a/sketch_apr3a.ino
@@ -1,23 +1,26 @@
-//#include <DallasTemperature.h>.
-//#include <OneWire.h>
+#include <DallasTemperature.h>.
+#include <OneWire.h>
 #include <SD.h>
 #include <MS5611.h>
 #include <AccelerometerV2.h>
 #include <Wire.h> 
 using namespace IntroSatLib;
 
-//float temp ;
+float temp ;
+
+String battery ;
 
 MS5611 ms5611;
 
-//OneWire oneWire(PA11);    
-//DallasTemperature sensors(&oneWire);
+OneWire oneWire(PA11);    
+DallasTemperature sensors(&oneWire);
 
 const int PIN_CHIP_SELECT = PA4; 
 File dataFile_1;
 File dataFile_2;
 
 AccelerometerV2 accel(Wire, 0x6B);
+
 
 
 void setup() {
@@ -32,16 +35,20 @@ void setup() {
   pinMode(PA4, OUTPUT); 
   if (!SD.begin(PIN_CHIP_SELECT)) {
    Serial.println("Card failed, or not present");
+   delay(100);
   } else {
     Serial.println("card initialized.");
+    delay(100);
   }
+  Serial.println("debug_1");
   if (!ms5611.begin(MS5611_ULTRA_HIGH_RES)) {
    Serial.println("MS5611 failed, or not present");
   } else {
     Serial.println("MS5611 initialized.");
   }
-//  pinMode(PB12, OUTPUT);
-//  sensors.begin();
+  Serial.println("debug_2");
+  pinMode(PB12, OUTPUT);
+  sensors.begin();
 }
 
 void loop() {
@@ -49,11 +56,12 @@ void loop() {
   dataFile_2 = SD.open("all.txt", FILE_WRITE);
   
   
-//  sensors.requestTemperatures(); 
-//  temp = (sensors.getTempCByIndex(0)); 
+  sensors.requestTemperatures(); 
+  temp = (sensors.getTempCByIndex(0)); 
   unsigned long currentTime = millis();
   double realTemperature = ms5611.readTemperature();
   long realPressure = ms5611.readPressure();
+  double realAltitude = ms5611.getAltitude(realPressure);
   delay(100);
   float acc_x = accel.X();
   delay(100);
@@ -62,37 +70,54 @@ void loop() {
   float acc_z = accel.Z();
   delay(100);
 
-  
-  Serial.print("0");  Serial.print(";");
-  Serial.print(currentTime);  Serial.print(";");
-  Serial.print("1000");  Serial.print(";");
-  Serial.print("6000");  Serial.print(";");
-  Serial.print("55.9297334");  Serial.print(";");
-  Serial.print("37.6173");  Serial.print(";");
-  Serial.print(realPressure);dataFile_1.print(realPressure);Serial.print(";");dataFile_1.print(";");
-  Serial.print(realTemperature);dataFile_1.print(realTemperature);Serial.print(";");dataFile_1.print(";");
-  Serial.print(acc_z);dataFile_1.print(acc_z);Serial.print(";");dataFile_1.print(";");
-  Serial.print("1000");dataFile_1.print("1000");Serial.print(";");dataFile_1.print(";");
-//  Serial.print(temp);dataFile_1.print(temp);Serial.print(";");dataFile_1.print(";");
-  Serial.print("1000");dataFile_1.print("1");Serial.print(";");dataFile_1.print(";");
-  Serial.print("1000");dataFile_1.print("0");Serial.print(";");dataFile_1.print(";");
-  Serial.print(acc_x);dataFile_1.print(acc_x);Serial.print(";");dataFile_1.print(";");
-  Serial.print(acc_y);dataFile_1.print(acc_y);Serial.print(";");dataFile_1.print(";");
-  Serial.print("1");dataFile_1.print("1");Serial.print(";");dataFile_1.print(";");
-  Serial.print("0");dataFile_1.print("0");Serial.print(";");dataFile_1.print(";");
-  
-//  if (temp >= 10.0){
-//  digitalWrite(PB12,LOW);
-//  Serial.print("0");            
-//  dataFile_1.print("0");
-//  }
-//  if (temp <= 15.0){
-//  digitalWrite(PB12, HIGH);
-//  Serial.print("1");
-//  dataFile_1.print("1");  
-//  }
-  
 
+
+  if (temp >= 10.0){
+  digitalWrite(PB12,LOW);
+  battery = ("0");
+  }
+  if (temp <= 15.0){
+  digitalWrite(PB12, HIGH);
+  battery = ("1");
+  }
+
+  
+Serial.println(String("0")+";"+
+                String(currentTime)+";"+
+                String("1000")+";"+
+                String("6000")+";"+
+                String("55.9297334")+";"+
+                String("37.6173")+";"+
+                String(realPressure)+";"+
+                String(realTemperature)+";"+
+                String(acc_z)+";"+
+                String("1000")+";"+ 
+                String(temp)+";"+
+                String("1000")+";"+
+                String("1000")+";"+
+                String(acc_x)+";"+
+                String(acc_y)+";"+
+                String("1")+";"+
+               String("0")+";"+String(battery)+";"+
+               String(realAltitude));
+dataFile_1.println(String("0")+";"+
+               String(currentTime)+";"+
+               String("1000")+";"+
+               String("6000")+";"+
+               String("55.9297334")+";"+
+               String("37.6173")+";"+
+               String(realPressure)+";"+
+               String(realTemperature)+";"+
+               String(acc_z)+";"+
+               String("1000")+";"+
+               String(temp)+";"+
+               String("1000")+";"+
+               String("1000")+";"+
+               String(acc_x)+";"+
+               String(acc_y)+";"+
+               String("1")+";"+
+               String("0")+";"+String(battery)+";"+
+               String(realAltitude));
 
 
   delay(1000); 
